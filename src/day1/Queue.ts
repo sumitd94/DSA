7438ced54b97ff4b8271b97b61447b37c74c0165
sumitd94 <sumitdhanania94@gmail.com>
2023-09-23 12:37:19 +0530

    implement queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,49 @@
+type Node<T> = {
+  value: T;
+  next?: Node<T>;
+};
+
 export default class Queue<T> {
-    public length: number;
+  public length: number;
+  private head: Node<T> | undefined;
+  private tail: Node<T> | undefined;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
+  enqueue(item: T): void {
+    const node = { value: item };
+
+    this.length++;
+    if (!this.tail) {
+      this.head = this.tail = node;
     }
 
-    enqueue(item: T): void {
+    this.tail.next = node;
+    this.tail = node;
+  }
+  deque(): T | undefined {
+    if (!this.head) return;
 
-}
-    deque(): T | undefined {
+    const head = this.head;
 
-}
-    peek(): T | undefined {
+    this.head = this.head.next;
+    this.length--;
 
-}
+    if (this.length === 0) {
+      this.head = this.tail = undefined;
+    }
+
+    // free up space
+    head.next = undefined;
+
+    return head.value;
+  }
+  peek(): T | undefined {
+    if (!this.head) return undefined;
+
+    return this.head?.value;
+  }
 }
